5ef345aaf7ff108ee65587d814a96eaf59c0a36e
Brooks Eshe <beshe@mail.greenriver.edu>
2019-03-05 23:48:19 -0500

    added a route to view student info

--- index.php
+++ index.php
@@ -7,9 +7,14 @@ error_reporting(E_ALL);
 require_once ('vendor/autoload.php');
 require_once ('model/db-functions.php');
 
+//start session after autoload
+session_start();
+
 //connect to database
 $dbh = connect();
-print_r($dbh);
+if(!$dbh){
+    exit;
+}
 
 //Create an instance of the Base class
 $f3 = Base::instance();
@@ -18,9 +23,53 @@ $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
 //Define a default route
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
+
+    $students = getStudents();
+
+    $f3->set('students',$students);
+
     $template = new Template();
     echo $template->render('views/all-students.html');
+});
+
+$f3->route('GET|POST /add', function($f3) {
+
+    if(isset($_POST['add'])){
+        $sid = $_POST['sid'];
+        $first = $_POST['first'];
+        $last = $_POST['last'];
+        $birth = $_POST['birth'];
+        $gpa = $_POST['gpa'];
+        $advisor = $_POST['advisor'];
+        $success = addStudent($sid,$first,$last,$birth,
+            $gpa,$advisor);
+
+        if($success){
+            //create a student object
+            $student = new Student($sid,$first,$last,$birth,
+                $gpa,$advisor);
+
+            //add to a session
+            $_SESSION['student'] = $student;
+
+            //reroute to home page
+            $f3->reroute('/');
+        }
+    }
+
+    $template = new Template();
+    echo $template->render('views/new-student.html');
+});
+
+$f3->route('GET /summary/@sid', function($f3, $params) {
+    $sid = $params['sid'];
+    $student = getStudent($sid);
+    $f3->set('student', $student);
+
+    //load a template
+    $template = new Template();
+    echo $template->render('views/view-student.html');
 });
 
 //Run fat-free
